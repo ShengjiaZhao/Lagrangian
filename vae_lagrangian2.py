da906ec36c4ee5b1e642edcdc976ead999490c1e
ShengjiaZhao <sjzhao@stanford.edu>
2018-03-08 10:53:32 -0800

    Fix bugs

--- vae_lagrangian2.py
+++ vae_lagrangian2.py
@@ -16,13 +16,13 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-g', '--gpu', type=str, default='1', help='GPU to use')
 parser.add_argument('-n', '--train_size', type=int, default=50000, help='Number of samples for training')
-parser.add_argument('-e1', '--epsilon1', type=float, default=114.0)
-parser.add_argument('-e2', '--epsilon2', type=float, default=0.15)
+parser.add_argument('-e1', '--epsilon1', type=float, default=112.0)
+parser.add_argument('-e2', '--epsilon2', type=float, default=0.4)
 parser.add_argument('-m', '--mi', type=float, default=0.0, help='Information Preference')
 parser.add_argument('-l1', '--lambda1', type=float, default=1.0)
 parser.add_argument('-l2', '--lambda2', type=float, default=10.0)
-parser.add_argument('-z', type=int, default=5)
-parser.add_argument('-t', type=str, default='cnn')
+parser.add_argument('-z', '--z', type=int, default=5)
+parser.add_argument('-t', '--t', type=str, default='cnn')
 parser.add_argument('--lagrangian', action='store_true')
 parser.add_argument('--no_eval', action='store_true')
 args = parser.parse_args()
@@ -33,7 +33,7 @@ batch_size = 200
 
 def make_model_path(name):
     if args.lagrangian:
-        log_path = os.path.join('log/lagrangian', name)
+        log_path = os.path.join('log/lagrangian_bn', name)
     else:
         log_path = os.path.join('log/infovae', name)
     if os.path.isdir(log_path):
@@ -53,10 +53,11 @@ else:
 def encoder(x, z_dim):
     if args.t == 'cnn':
         with tf.variable_scope('encoder'):
-            conv = conv2d_lrelu(x, 64, 4, 2)   # None x 14 x 14 x 64
-            conv = conv2d_lrelu(conv, 128, 4, 2)   # None x 7 x 7 x 128
+            conv = conv2d_bn_lrelu(x, 64, 4, 2)   # None x 14 x 14 x 64
+            conv = conv2d_bn_lrelu(conv, 64, 4, 1)
+            conv = conv2d_bn_lrelu(conv, 128, 4, 2)   # None x 7 x 7 x 128
             conv = tf.reshape(conv, [-1, np.prod(conv.get_shape().as_list()[1:])]) # None x (7x7x128)
-            fc = fc_lrelu(conv, 1024)
+            fc = fc_bn_lrelu(conv, 1024)
             mean = tf.contrib.layers.fully_connected(fc, z_dim, activation_fn=tf.identity)
             stddev = tf.contrib.layers.fully_connected(fc, z_dim, activation_fn=tf.sigmoid)
             stddev = tf.maximum(stddev, 0.05)
@@ -79,12 +80,13 @@ def decoder(z, reuse=False):
         with tf.variable_scope('decoder') as vs:
             if reuse:
                 vs.reuse_variables()
-            fc = fc_relu(z, 1024)
-            fc = fc_relu(fc, 7*7*128)
+            fc = fc_bn_relu(z, 1024)
+            fc = fc_bn_relu(fc, 7*7*128)
             conv = tf.reshape(fc, tf.stack([tf.shape(fc)[0], 7, 7, 128]))
-            conv = conv2d_t_relu(conv, 64, 4, 2)
+            conv = conv2d_t_bn_relu(conv, 64, 4, 2)
+            conv = conv2d_t_bn_relu(conv, 64, 4, 1)
             mean = tf.contrib.layers.convolution2d_transpose(conv, 1, 4, 2, activation_fn=tf.sigmoid)
-            mean = tf.maximum(tf.minimum(mean, 0.995), 0.005)
+            mean = tf.maximum(tf.minimum(mean, 0.99), 0.01)
             return mean
     else:
         with tf.variable_scope('decoder', reuse=reuse):
@@ -92,7 +94,7 @@ def decoder(z, reuse=False):
             fc2 = fc_tanh(fc1, 1024)
             logits = tf.contrib.layers.fully_connected(fc2, 784, activation_fn=tf.identity)
             logits = tf.reshape(logits, [-1, 28, 28, 1])
-            return tf.minimum(tf.maximum(tf.sigmoid(logits), 0.005), 0.995)
+            return tf.minimum(tf.maximum(tf.sigmoid(logits), 0.01), 0.99)
 
 
 # Build the computation graph for training
@@ -129,7 +131,7 @@ def compute_mmd(x, y):   # [batch_size, z_dim] [batch_size, z_dim]
 
 # Compare the generated z with true samples from a standard Gaussian, and compute their MMD distance
 true_samples = tf.random_normal(tf.stack([batch_size, z_dim]))
-loss_mmd = compute_mmd(true_samples, train_z) * 10.0
+loss_mmd = compute_mmd(true_samples, train_z) * 50.0
 
 # ELBO loss divided by input dimensions
 elbo_per_sample = tf.reduce_sum(-tf.log(train_zstddev) + 0.5 * tf.square(train_zstddev) +
@@ -149,11 +151,8 @@ else:
     lambda2 = tf.get_variable('lambda2', shape=[], dtype=tf.float32, initializer=tf.constant_initializer(args.lambda2))
 
 # Cannot simply use clipping here because once at max/min there is no more gradient
-lambda1_ub = tf.assign(lambda1, tf.minimum(lambda1, 200.0))
-lambda1_lb = tf.assign(lambda1, tf.maximum(lambda1, 0.0))
-lambda2_ub = tf.assign(lambda2, tf.minimum(lambda2, 200.0))
-lambda2_lb = tf.assign(lambda2, tf.maximum(lambda2, 0.0))
-lambda_clip = [lambda1_ub, lambda1_lb, lambda2_ub, lambda2_lb]
+lambda1_clip = tf.assign(lambda1, tf.maximum(tf.minimum(lambda1, 200.0), 0.0))
+lambda2_clip = tf.assign(lambda2, tf.maximum(tf.minimum(lambda2, 200.0), 0.0))
 
 loss_all = lambda1 * (loss_nll + loss_elbo) + lambda2 * loss_mmd \
            - args.epsilon1 * lambda1 - args.epsilon2 * lambda2
@@ -167,13 +166,14 @@ model_vars = [var for var in tf.global_variables() if 'encoder' in var.name or '
 # optimizer = tf.train.AdamOptimizer(1e-4)
 # grads = tf.gradients(loss_all, model_vars)
 trainer = tf.train.AdamOptimizer(1e-4).minimize(loss_all, var_list=model_vars)
-lambda_update = tf.train.GradientDescentOptimizer(1e-3).minimize(-loss_all, var_list=lambda_vars)
+lambda_update = tf.train.GradientDescentOptimizer(5e-3).minimize(-loss_all, var_list=lambda_vars)
 
 limited_mnist = LimitedMnist(args.train_size)
 
 train_summary = tf.summary.merge([
-    tf.summary.scalar('elbo', loss_elbo),
+    tf.summary.scalar('zkl', loss_elbo),
     tf.summary.scalar('nll', loss_nll),
+    tf.summary.scalar('elbo', loss_elbo + loss_nll),
     tf.summary.scalar('mmd', loss_mmd),
     tf.summary.scalar('loss', loss_all),
     tf.summary.scalar('lambda1', lambda1),
@@ -279,7 +279,7 @@ for i in range(1, 1000000):
         sess.run([trainer, lambda_update], feed_dict={train_x: bx})
     else:
         sess.run(trainer, feed_dict={train_x: bx})
-    sess.run(lambda_clip)
+    sess.run([lambda1_clip, lambda2_clip])
 
     if i % 100 == 0:
         merged = sess.run(train_summary, feed_dict={train_x: bx})
